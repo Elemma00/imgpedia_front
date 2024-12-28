d14195da07b5067f74ad9b8d2b444aa276c214aa
Elemma00 <faundez76@gmail.com>
2024-12-28 16:54:11 -0300

    Query and Details Components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { QueryComponent } from './components/query/query.component';
+import { DetailsComponent } from './components/details/details.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
+    { path: 'query', component: QueryComponent },
+    { path: 'query/:q', component: QueryComponent },
+    { path: 'detail/:filename', component: DetailsComponent }
 ];
