0ee5e4cc99f1a9983503183af82c16b09722705a
Elemma00 <faundez76@gmail.com>
2024-12-28 14:07:27 -0300

    Landing page in progress

---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../shared/navbar/navbar.component";
+
+@Component({
+  selector: 'imgpedia-main',
+  imports: [NavbarComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+}
