96d745026907709addcc0719ba055a11b2aa201d
Elemma00 <faundez76@gmail.com>
2025-04-21 13:47:16 -0400

    cambios en el tamaño máximo

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    IMGPEDIA_API_URL: 'http://localhost:8080/api',
-    IMGPEDIA_API_QUERY_URL: 'http://localhost:8080/api/sparql/query',
+    IMGPEDIA_API_URL: 'http://localhost:8081/api',
+    IMGPEDIA_API_QUERY_URL: 'http://localhost:8081/api/sparql/query',
   };
